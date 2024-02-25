37fa1d50a2dc7939848b02c205caad9bcbe20957
danhoangg <hoang.daniel2005@gmail.com>
2024-02-25 17:18:36 +0000

    app can now run on ios phone

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
+  // Firebase Auth instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Firestore instance
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // sign user in
   Future<UserCredential> signInWithEmailandPassword(
@@ -12,6 +17,12 @@ class AuthService extends ChangeNotifier {
       // sign in
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      // add new document in case it doesnt exist
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'email': email,
+        'uid': userCredential.user!.uid,
+      }, SetOptions(merge: true));
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
@@ -26,6 +37,12 @@ class AuthService extends ChangeNotifier {
       // register user
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // create new document in firestore
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'email': email,
+        'uid': userCredential.user!.uid,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
